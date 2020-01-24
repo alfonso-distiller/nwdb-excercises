a28ba637b08740af8cec28171857f68785a38313
Carlos Alfonso del Rio Juarez <carlos.rio@softtek.com>
2020-01-24 12:23:22 -0600

    formatting fixes

--- 02_monthly_sales_by_state.sql
+++ 02_monthly_sales_by_state.sql
@@ -1,12 +1,13 @@
 -- Create Sales Report by year, month, by state
 -- Show total revenue - List Price * order quantity
 -- Limit to order lines invoiced
-select date_format(o.order_date, '%Y - %M') as 'Sales Month',
-o.ship_state_province as 'State',
-concat('$ ',format((od.quantity * p.list_price), 2))  
+select 
+	date_format(o.order_date, '%Y - %M') as 'Sales Month',
+	o.ship_state_province as 'State',
+	concat('$ ',format((od.quantity * p.list_price), 2))  
 from orders o
-join order_details od on o.id = od.order_id
-join products p on od.product_id = p.id
-where od.status_id = 2
-GROUP BY date_format(order_date, '%Y - %m'), o.ship_state_province
-ORDER BY 'Sales Month' ASC;
+	join order_details od on o.id = od.order_id
+	join products p on od.product_id = p.id
+	where od.status_id = 2
+	group by date_format(order_date, '%Y - %m'), o.ship_state_province
+	order by 'Sales Month' asc;
